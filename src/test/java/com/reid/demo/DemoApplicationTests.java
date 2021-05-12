5e8d8342211f23b4c0a03d8a7eb45c0a5de4f440
tayxxs <tayxxs@users.noreply.github.com>
2021-05-12 23:24:26 +0800

    初始化

---
+++ src/test/java/com/reid/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
