2a3602a37e21ab1a63fc4bd36d1a6e84a88584c3
tayxxs <tayxxs@users.noreply.github.com>
2021-05-18 22:42:24 +0800

    基础功能测试

---
+++ src/main/java/com/reid/demo/DatabaseLoader.java
@@ -0,0 +1,20 @@
+package com.reid.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ringbearer"));
+    }
+    
+}
