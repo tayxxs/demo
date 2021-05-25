77a78c72299f44aec495349bb17b57df5573588d
tayxxs <tayxxs@users.noreply.github.com>
2021-05-25 20:12:30 +0800

    基础功能rest功能完成

--- src/main/java/com/reid/demo/DatabaseLoader.java
+++ src/main/java/com/reid/demo/DatabaseLoader.java
@@ -17,6 +17,11 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new Employee("Frodo", "Baggins", "ringbearer"));
+        this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
+		this.repository.save(new Employee("Gandalf", "the Grey", "wizard"));
+		this.repository.save(new Employee("Samwise", "Gamgee", "gardener"));
+		this.repository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
+		this.repository.save(new Employee("Peregrin", "Took", "pipe smoker"));
     }
     
 }
